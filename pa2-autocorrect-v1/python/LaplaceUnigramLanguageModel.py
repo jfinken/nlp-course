774c823864362c002d6de7cd5e51ea27d674f3b7
Josh Finken <josh@earthmine.com>
2012-04-06 17:05:02 -0700

    inital notes on implementing the laplace unigram model

--- pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
+++ pa2-autocorrect-v1/python/LaplaceUnigramLanguageModel.py
@@ -2,19 +2,31 @@ class LaplaceUnigramLanguageModel:
 
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
-    # TODO your code here
+    self.words = set([][])
     self.train(corpus)
 
   def train(self, corpus):
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
-    pass
+    for sentence in corpus.corpus: # iterate over sentences in the corpus
+      for datum in sentence.data: # iterate over datums in the sentence
+        word = datum.word # get the word
+
+        # computing C(word) + 1:
+
+        # lookup the word, if exists, increment count for that word
+        # else
+          #self.words.add(word)
+          #increment
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
+    for token in sentence: # iterate over words in the sentence
+      # compute: (C(w) + 1) / (N + V), retrieving C(w) from trained matrix
+      #probability = math.log(1.0/len(self.words))
+      #score += probability
+
     return 0.0
